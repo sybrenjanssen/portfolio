dd25ecf194ceefffa1f33a95dbe084e9dad2fbb8
Sybren Janssen <sybren1709@gmail.com>
2020-04-18 22:49:35 +0200

    Added menu animations

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -16,7 +16,7 @@ import { trigger, animate, transition, style, query, animateChild, stagger } fro
           style({
               opacity: 0,
               transform: 'scale(0.5)'
-          }), animate('300ms ease-in', style({
+          }), animate('500ms cubic-bezier(.36,0,.63,1)', style({
               opacity: 1,
               transform: 'scale(1)'
           }))
@@ -26,9 +26,89 @@ import { trigger, animate, transition, style, query, animateChild, stagger } fro
               opacity: 1,
               transform: 'scale(1)'
           }),
-          animate('300ms ease-in', style({
+          animate('500ms cubic-bezier(.36,0,.63,1)', style({
               opacity: 0,
               transform: 'scale(0.5)'
+          }))
+        ])
+      ]),
+      trigger('firstMenuItem', [
+        transition('void => *', [
+          style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }), animate('550ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }))
+        ]),
+        transition('* => void', [
+          style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }), animate('350ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }))
+        ])
+      ]),
+      trigger('secondMenuItem', [
+        transition('void => *', [
+          style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }), animate('650ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }))
+        ]),
+        transition('* => void', [
+          style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }), animate('250ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }))
+        ])
+      ]),
+      trigger('thirdMenuItem', [
+        transition('void => *', [
+          style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }), animate('750ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }))
+        ]),
+        transition('* => void', [
+          style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }), animate('150ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 0,
+              transform: 'translateY(-100px)'
+          }))
+        ])
+      ]),
+      trigger('footer', [
+        transition('void => *', [
+          style({
+              opacity: 0,
+              transform: 'translateY(300px)'
+          }), animate('600ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }))
+        ]),
+        transition('* => void', [
+          style({
+              opacity: 1,
+              transform: 'translateY(0)'
+          }), animate('600ms cubic-bezier(.36,0,.63,1)', style({
+              opacity: 0,
+              transform: 'translateY(800px)'
           }))
         ])
       ])

a17263e6dbb9b0430a4245adb55cdee64d9e1bae
Sybren Janssen <sybren1709@gmail.com>
2020-04-18 16:29:06 +0200

    Changed menu + animations

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, animate, transition, style, query, animateChild, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
+  animations: [
+    trigger('ngIfAnimation', [
+      transition(':enter, :leave', [
+        query('@*', animateChild())
+      ])
+    ]),
+    trigger('easeInOut', [
+      transition('void => *', [
+          style({
+              opacity: 0,
+              transform: 'scale(0.5)'
+          }), animate('300ms ease-in', style({
+              opacity: 1,
+              transform: 'scale(1)'
+          }))
+      ]),
+      transition('* => void', [
+          style({
+              opacity: 1,
+              transform: 'scale(1)'
+          }),
+          animate('300ms ease-in', style({
+              opacity: 0,
+              transform: 'scale(0.5)'
+          }))
+        ])
+      ])
+  ]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  toggled = false;
 
   ngOnInit() {
-
   }
 
+  toggleMenu() {
+    this.toggled = !this.toggled;
+ 
+
+  }
 }
